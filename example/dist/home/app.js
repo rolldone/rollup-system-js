244ad6f21fda1a147866b5255bded93097b2c575
rolldone <donny.rollproject@gmail.com>
2021-12-27 11:03:11 +0800

    First commit

--- example/dist/home/app.js
+++ example/dist/home/app.js
@@ -1,11 +1,9 @@
-System.register([], (function (exports) {
+System.register([], (function () {
   'use strict';
   return {
     execute: (function () {
 
-      exports('default', app);
 
-      function app(){setTimeout(function(){document.querySelector("h1").innerHTML="Is Changed!!",alert("changed");},5e3);}
 
     })
   };
